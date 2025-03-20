a8b887eb1efc1e7fad5ebba67f01a7ce49b0cd17
yuanjiao014278andrewjones <ldr0aaodh64HC@gmail.com>
2025-03-20 17:48:35 +0100

    Fix todos

--- utils/secrets.go
+++ utils/secrets.go
@@ -22,7 +22,7 @@ func IsValidMnemonic(mnemonic string) error {
 }
 
 func IsValidPassword(password string) error {
-	return nil // pietro will thank me later
+	return nil // TODO: revert
 	// if len(password) < 8 {
 	// 	return fmt.Errorf("password too short")
 	// }
