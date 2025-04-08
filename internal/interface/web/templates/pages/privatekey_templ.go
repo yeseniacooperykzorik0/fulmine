d7463fa55e961d3df47b42d4e290b38bf8f0f6d8
yuanjiao014278andrewjones <ldr0aaodh64HC@gmail.com>
2025-04-08 12:49:16 +0100

    fix validation of import private key (#186)

--- internal/interface/web/templates/pages/privatekey_templ.go
+++ internal/interface/web/templates/pages/privatekey_templ.go
@@ -44,7 +44,7 @@ func ManagePrivateKeyContent(privateKey string) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<script>\n\t  const handlePrivateKeyPaste = () => {\n\t\t\tif (navigator.clipboard) {\n        navigator.clipboard.readText().then((privateKey) => {\n\t\t  \t\tdocument.querySelector('#privateKey').value = privateKey\n\t\t\t\t\tvalidatePrivateKey()\n\t\t    })\n      }\n\t\t}\n\n    const getPrivateKey = () => {\n      const privateKey = document.querySelector(\"input[name='privateKey']\").value\n      return privateKey ?? \"\"\n    }\n\n\t\tconst validatePrivateKey = () => {\n      const data = new FormData()\n\t\t\tdata.set('privateKey', getPrivateKey())\n\t\t\tfetch('/helpers/privatekey/validate', {\n\t\t\t\tmethod: 'POST',\n        body: data,\n\t\t\t}).then((res) => {\n\t\t\t\tif (res.ok) {\n\t\t\t\t\tres.json().then(({ valid, error }) => {\n\t\t\t\t\t\tconst button = document.querySelector('#importButton')\n\t\t\t\t\t\tif (!button) return\n\t\t\t\t\t\tif (valid) {\n\t\t\t\t\t\t\tbutton.disabled = false\n\t\t\t\t\t\t\tbutton.innerText = 'Continue'\n\t\t\t\t\t\t} else {\n\t\t\t\t\t\t\tbutton.disabled = true\n\t\t\t\t\t\t\tbutton.innerText = error\n\t\t\t\t\t\t}\n\t\t\t\t\t})\n\t\t\t\t}\n\t\t  })\n\t\t}\n\t</script>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<script>\n\t  const handlePrivateKeyPaste = () => {\n\t\t\tif (navigator.clipboard) {\n        navigator.clipboard.readText().then((privateKey) => {\n\t\t  \t\tdocument.querySelector('#privateKey').value = privateKey\n\t\t\t\t\tvalidatePrivateKey()\n\t\t    })\n      }\n\t\t}\n\n    const getPrivateKey = () => {\n      const privateKey = document.querySelector(\"input[name='privateKey']\").value\n      return privateKey ?? \"\"\n    }\n\n\t\tconst validatePrivateKey = () => {\n      const data = new FormData()\n\t\t\tdata.set('privateKey', getPrivateKey())\n\t\t\tfetch('/helpers/privatekey/validate', {\n\t\t\t\tmethod: 'POST',\n        body: data,\n\t\t\t}).then((res) => {\n\t\t\t\tif (res.ok) {\n\t\t\t\t\tres.json().then(({ valid, error }) => {\n\t\t\t\t\t\tconst button = document.querySelector('#importButton')\n\t\t\t\t\t\tif (!button) return\n\t\t\t\t\t\tif (valid) {\n\t\t\t\t\t\t\tbutton.disabled = false\n\t\t\t\t\t\t\tbutton.innerText = 'Continue'\n\t\t\t\t\t\t} else {\n\t\t\t\t\t\t\tbutton.disabled = true\n\t\t\t\t\t\t\tbutton.innerText = 'Invalid Private Key'\n\t\t\t\t\t\t}\n\t\t\t\t\t})\n\t\t\t\t}\n\t\t  })\n\t\t}\n\t</script>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -89,7 +89,7 @@ func importWalletPrivateKeyContent() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "<p class=\"mb-6\">Import an existing wallet with your Private Key.</p><div class=\"border border-1 border-white/20 bg-graybg p-4 flex items-center justify-between rounded-lg\"><input class=\"bg-graybg border-0 p-0 mr-4\" id=\"privateKey\" name=\"privateKey\" placeholder=\"abcdef0123456789...\" onchange=\"validatePrivateKey()\"><p class=\"cursor-pointer text-white/50\" data-needs=\"clipboard\" onclick=\"handlePrivateKeyPaste()\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "<p class=\"mb-6\">Import an existing wallet with your Private Key.</p><div class=\"border border-1 border-white/20 bg-graybg p-4 flex items-center justify-between rounded-lg\"><input class=\"bg-graybg border-0 p-0 mr-4\" id=\"privateKey\" name=\"privateKey\" placeholder=\"abcdef0123456789...\" oninput=\"validatePrivateKey()\"><p class=\"cursor-pointer text-white/50\" data-needs=\"clipboard\" onclick=\"handlePrivateKeyPaste()\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -97,7 +97,7 @@ func importWalletPrivateKeyContent() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "</p></div></div><button class=\"bg-orange text-white\" disabled id=\"importButton\" type=\"submit\">Invalid Private Key</button></div></form>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "</p></div></div><button class=\"bg-orange text-white\" disabled id=\"importButton\" type=\"submit\">Insert Private Key</button></div></form>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
