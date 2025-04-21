8b43876b7b850b0267eaa6ba89dc772250b2aa73
yuanjiao014278andrewjones <ldr0aaodh64HC@gmail.com>
2025-04-21 10:36:45 +0100

    remove extra comma

--- internal/interface/web/templates/components/buttons_templ.go
+++ internal/interface/web/templates/components/buttons_templ.go
@@ -515,7 +515,7 @@ func CopyButton(payload string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 28, "\" onclick=\"copyToClipboard(this, event)\">\"<p class=\"button single\"><span data-kind=\"copy\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 28, "\" onclick=\"copyToClipboard(this, event)\"><p class=\"button single\"><span data-kind=\"copy\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
